fb142b55c18be544c87922c37e4972b4bb2268b1
Pedro Daniel <pedro-edu19@outlook.com>
2023-05-08 18:08:09 -0300

    add code

---
+++ bot.py
@@ -0,0 +1,82 @@
+import os
+import time
+import re
+
+from chatterbot.trainers import ListTrainer
+from chatterbot import ChatBot
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class Wppbot:
+	# pega caminho da aplicação
+	dir_path = os.getcwd()
+
+	# recebe o nome do bot
+	def __init__(self, name_bot):
+		self.bot = ChatBot(name_bot)
+		# seta como o bot irar treinar
+		#self.bot.set_trainer(ListTrainer)
+		self.trainer = ListTrainer(self.bot)
+		# seta nde esta o driver
+		self.chrome = self.dir_path+'\\chromedriver.exe'
+
+		#configurarmos m profile no chrome para não precivsar logar no wp sempre que abrir o bot
+		self.options = webdriver.ChromeOptions()
+		self.options.add_argument(r"user-data-dir="+self.dir_path+"\profile\wpp")
+		self.driver = webdriver.Chrome(self.chrome, chrome_options=self.options)
+
+	def iniciar(self, nome_contato):
+		# entra no link so wp
+		self.driver.get('https://web.whatsapp.com/')
+		self.driver.implicitly_wait(15)# aguarda 15 segundos
+
+		# pega elemento da caixa de pesquisa
+
+		self.caixa_pesquisa = self.driver.find_element(By.CLASS_NAME, 'iq0m558w')
+		self.caixa_pesquisa.send_keys(nome_contato)# busca pelo nome do contato
+		time.sleep(2)
+
+		# procura contato/grupo que esta na tag span e possui o nome que pesquisamos eclicamos nele
+		self.contato = self.driver.find_element(By.XPATH, '//span[@title = "{}"]'.format(nome_contato))
+		self.contato.click()
+		time.sleep(2)
+
+	
+	def saudacoe(self, frase_inicial):
+		self.caixa_menssagen = self.driver.find_element(By.CLASS_NAME, '_2lSWV')
+
+		if type(frase_inicial) == list:
+			for frase in frase_inicial:
+				self.caixa_menssagen.send_keys(frase)
+				time.sleep(1)
+				self.botao_enviar = self.driver.find_element(By.CLASS_NAME, 'tvf2evcx')
+				self.botao_enviar.click()
+				time.sleep(1)
+		else:
+			return False
+
+
+	def escuta(self):
+		post = self.driver.find_element_by_class_name('_3_7SH')
+		ultimo = len(post) - 1
+		texto = post[ultimo].find_element_by_css_selector('span.selectable-text').text
+		return texto
+
+
+	def responde(self, texto):
+		response = self.bot.get_response(texto)
+		response = str(response)
+		response = 'bot:' + response
+
+		self.caixa_menssagen = self.find_element_by_class_name('_2S1VP')
+		self.caixa_menssagen.send_keys(response)
+		time.sleep(1)
+		self.botao_enviar = self.find_element_by_class_name('_35EW6')
+		self.botao_enviar.click()
+
+
+	def treinar(self, nome_pasta):
+		for treino in os.listdir(nome_pasta):
+			conversas = open(nome_pasta+'/'+treino, 'r').readlines()
+			self.trainer.train(conversas)
---
+++ main.py
@@ -0,0 +1,8 @@
+import re
+from bot import Wppbot
+
+bot = Wppbot('robot')
+
+bot.treinar('treino')
+bot.iniciar('Meu Amor')
+bot.saudacoe(['Bot: oi tudo bem', 'Bot: Como vc esta'])
